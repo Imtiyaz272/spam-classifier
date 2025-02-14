6f8e4af8c03edb04a1ed818182765103f4e4c727
Imtiyaz272 <syedaimtiyaz19@gmail.com>
2025-02-14 16:34:21 +0530

    Update backend

--- backend/model_predict.py
+++ backend/model_predict.py
@@ -2,14 +2,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
 
-# Load the trained model and vectorizer
 with open("model.pkl", "rb") as model_file:
     model = pickle.load(model_file)
 with open("vectorizer.pkl", "rb") as vectorizer_file:
     vectorizer = pickle.load(vectorizer_file)
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for frontend access
+CORS(app)  
 
 @app.route('/predict', methods=['POST'])
 def predict():
